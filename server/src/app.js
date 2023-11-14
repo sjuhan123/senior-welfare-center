f20d1d4aedb70781cec7cbb005031b1d64547352
sjuhan123 <sjuhan123@gmail.com>
2023-11-14 18:26:53 +0900

    ✨ server/feat: 로그 미들웨어 기능 구현
    - morgan 설치

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url"; // URL에서 파일 경로로 변환하는 함수
+import morgan from "morgan";
 
 import api from "./routes/api.js";
 
@@ -16,6 +17,10 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+
+// 로그 미들웨어
+app.use(morgan("combined"));
+
 app.use(express.json());
 // 프론트엔드 정적 파일 제공
 app.use(express.static(path.join(__dirname, "..", "public")));
