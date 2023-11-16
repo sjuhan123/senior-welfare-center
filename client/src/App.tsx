197976b544b99f55c29b3fd009b155e4b6823369
sjuhan123 <sjuhan123@gmail.com>
2023-11-16 15:27:09 +0900

    ✨ client/feat: chakra Ui theme 및 css reset 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import Routers from "./router";
+import { CSSReset, ChakraProvider } from "@chakra-ui/react";
+import theme from "./styles/theme";
 
 const App = () => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Routers />
-    </QueryClientProvider>
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <QueryClientProvider client={queryClient}>
+        <Routers />
+      </QueryClientProvider>
+    </ChakraProvider>
   );
 };
 
---
+++ client/src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: "Noto Serif KR, serif",
+    heading: "Noto Serif KR, serif",
+  },
+});
+
+export default theme;
