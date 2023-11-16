9b84b273ea9fe0be791111857da3495b6fd2473a
sjuhan123 <sjuhan123@gmail.com>
2023-11-16 14:49:29 +0900

    🔧 all/chore: style library 의존성 설치 및 svg 관련 의존성 설치
    - 의존성 관련 config 추가

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   build: {
     outDir: "../server/public", // 빌드 결과물의 경로를 설정합니다.
   },
