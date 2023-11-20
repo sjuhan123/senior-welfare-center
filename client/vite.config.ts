34b8349e0059ae2ea2c5e480b871ab76c85dd82a
sjuhan123 <sjuhan123@gmail.com>
2023-11-20 13:08:28 +0900

    ✅ client/test: client testing을 위한 의존성 설치 및 관련 config 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+  },
   build: {
     outDir: "../server/public", // 빌드 결과물의 경로를 설정합니다.
   },
