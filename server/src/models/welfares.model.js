4b77f73f1ae2c4ff4e82d104d1227d92204ad180
sjuhan123 <sjuhan123@gmail.com>
2023-11-21 20:28:05 +0900

    ✨ server/feat: /welfares endpoint District Schema와 연동 및 ?district params 로직 구현

--- server/src/models/welfares.model.js
+++ server/src/models/welfares.model.js
@@ -1,54 +1,87 @@
 import welfaresInfo from "../../data/welfares.data.js";
+import { saveDistrict } from "./districts.model.js";
+import District from "./districts.mongo.js";
 import Welfare from "./welfares.mongo.js";
 
 async function loadWelfareData() {
   try {
     for (const welfare of welfaresInfo) {
-      await savePlanet(welfare);
+      await saveWelfare(welfare);
     }
   } catch (error) {
     console.error("Error loading welfare data", error);
-    reject(error);
+    throw error;
   } finally {
     const welfaresFound = (await getAllWelfares()).length;
     console.info(`${welfaresFound} welfares found`);
   }
 }
 
-async function getAllWelfares(req, res) {
-  return await Welfare.find(
-    {},
-    {
-      _id: 0,
-      __v: 0,
-    }
-  );
-}
-
-async function savePlanet(welfare) {
+async function saveWelfare(welfare) {
   try {
-    const welfareInstance = new Welfare({
-      district: welfare.district,
-      name: welfare.name,
-      address: welfare.address,
-      phone: welfare.phone,
-      remarks: welfare.remarks,
-    });
+    const existingDistrict = await District.findOne({ name: welfare.district });
 
-    const filter = { district: welfareInstance.name };
-    const update = {
-      $set: {
-        name: welfareInstance.name,
-        address: welfareInstance.address,
-        phone: welfareInstance.phone,
-        remarks: welfareInstance.remarks,
-      },
-    };
+    if (existingDistrict) {
+      const welfareInstance = new Welfare({
+        district: existingDistrict._id,
+        name: welfare.name,
+        address: welfare.address,
+        phone: welfare.phone,
+        remarks: welfare.remarks,
+      });
 
-    await Welfare.updateOne(filter, update, { upsert: true });
+      const filter = { address: welfareInstance.address };
+      const update = {
+        $set: {
+          district: welfareInstance.district,
+          name: welfareInstance.name,
+          address: welfareInstance.address,
+          phone: welfareInstance.phone,
+          remarks: welfareInstance.remarks,
+        },
+      };
+
+      await Welfare.updateOne(filter, update, { upsert: true });
+    } else {
+      const districtId = await saveDistrict(welfare.district);
+
+      const welfareInstance = new Welfare({
+        district: districtId,
+        name: welfare.name,
+        address: welfare.address,
+        phone: welfare.phone,
+        remarks: welfare.remarks,
+      });
+
+      const filter = { address: welfareInstance.address };
+      const update = {
+        $set: {
+          district: welfareInstance.district,
+          name: welfareInstance.name,
+          address: welfareInstance.address,
+          phone: welfareInstance.phone,
+          remarks: welfareInstance.remarks,
+        },
+      };
+
+      await Welfare.updateOne(filter, update, { upsert: true });
+    }
   } catch (error) {
     console.error("Could not save welfare", error);
   }
 }
 
-export { loadWelfareData, getAllWelfares };
+async function getWelfaresBy(districtId) {
+  return await Welfare.find({ district: districtId }, "-__v").populate(
+    "district",
+    "name -_id"
+  );
+}
+
+async function getAllWelfares() {
+  return await Welfare.find({}, { _id: 0, __v: 0 }).populate("district", {
+    __v: 0,
+  });
+}
+
+export { loadWelfareData, getWelfaresBy, getAllWelfares };
--- server/src/routes/welfares/welfares.controller.js
+++ server/src/routes/welfares/welfares.controller.js
@@ -1,14 +1,26 @@
-import { getAllWelfares } from "../../models/welfares.model.js";
+import { getAllWelfares, getWelfaresBy } from "../../models/welfares.model.js";
 
 async function httpGetAllWelfares(req, res) {
   try {
-    const welfaresData = await getAllWelfares();
-    const jsonResponse = {
-      statusCode: 200,
-      message: "복지관 목록 조회 성공",
-      data: welfaresData,
-    };
-    return res.status(200).json(jsonResponse);
+    const { districtId } = req.query;
+
+    if (districtId) {
+      const welfaresData = await getWelfaresBy(districtId);
+      const jsonResponse = {
+        statusCode: 200,
+        message: "복지관 목록 조회 성공",
+        data: welfaresData,
+      };
+      return res.status(200).json(jsonResponse);
+    } else {
+      const welfaresData = await getAllWelfares();
+      const jsonResponse = {
+        statusCode: 200,
+        message: "복지관 목록 조회 성공",
+        data: welfaresData,
+      };
+      return res.status(200).json(jsonResponse);
+    }
   } catch (error) {
     console.error("Error retrieving welfares:", error);
     return res.status(500).json({
