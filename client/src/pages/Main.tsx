46db486666dafdea7d3454b6fa732f4c6ab0a217
sjuhan123 <sjuhan123@gmail.com>
2023-11-22 20:22:03 +0900

    ✨ client/feat: Main 페이지 CSS 일부 수정 및 LoginArea 연동

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,12 +1,26 @@
 import Layout from "../components/Layout";
 import Header from "../components/Header";
 import Contents from "../components/Contents";
+import Footer from "../components/Footer";
+import LoginArea from "../components/LoginArea";
+import { Box } from "@chakra-ui/react";
 
 const Main = () => {
   return (
     <Layout>
       <Header />
-      <Contents />
+      <Box
+        as="main"
+        w="100%"
+        display="flex"
+        flexDir="column"
+        gap="30px"
+        p="10px 0px"
+      >
+        <Contents />
+        <LoginArea />
+      </Box>
+      <Footer />
     </Layout>
   );
 };
