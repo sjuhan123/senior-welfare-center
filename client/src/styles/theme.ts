c45c5af558ab6397df3a950d97121ebf47620f83
sjuhan123 <sjuhan123@gmail.com>
2023-11-27 00:48:59 +0900

    ✨ client/feat: Modal 컴포넌트 기본 디자인 적용

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,9 +1,37 @@
 import { extendTheme } from "@chakra-ui/react";
+import { modalAnatomy as parts } from "@chakra-ui/anatomy";
+import { createMultiStyleConfigHelpers } from "@chakra-ui/styled-system";
+
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(parts.keys);
+
+const baseStyle = definePartsStyle({
+  overlay: {
+    bg: "blackAlpha.200",
+  },
+  dialog: {
+    top: "19%",
+    borderRadius: "md",
+    width: "80%",
+    height: "60%",
+  },
+  body: {
+    height: "100%",
+    overflowY: "scroll",
+  },
+});
+
+export const modalTheme = defineMultiStyleConfig({
+  baseStyle,
+});
 
 const theme = extendTheme({
   fonts: {
     body: "맑은 고딕, Malgun Gothic, sans-serif",
     heading: "맑은 고딕, Malgun Gothic, sans-serif",
+  },
+  components: {
+    Modal: modalTheme,
   },
 });
 
