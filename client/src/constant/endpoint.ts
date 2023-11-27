16efbe68aae4c1f107cdcdf5b5f33f32936f9088
sjuhan123 <sjuhan123@gmail.com>
2023-11-28 01:56:52 +0900

    🐛 client/fix: Logout endpoint 오기입 수정

--- client/src/constant/endpoint.ts
+++ client/src/constant/endpoint.ts
@@ -3,7 +3,7 @@ export const END_POINT = Object.freeze({
   DISTRICTS: `${import.meta.env.VITE_BASE_URL}/districts`,
   USER: `${import.meta.env.VITE_BASE_URL}/user`,
   KAKAO_LOGIN: `${import.meta.env.VITE_BASE_URL}/auth/kakao`,
-  KAKAO_LOGOUT: `${import.meta.env.VITE_BASE_URL}/auth/user/logout`,
+  KAKAO_LOGOUT: `${import.meta.env.VITE_BASE_URL}/user/logout`,
   USER_WELFARE_BOOKMARK: `${import.meta.env.VITE_BASE_URL}/user/welfare`,
   WELFARES: `${import.meta.env.VITE_BASE_URL}/welfares`,
 });
