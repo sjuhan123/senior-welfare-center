f1298f9bace7e54b755add16326ec6f7a300e100
sjuhan123 <sjuhan123@gmail.com>
2023-11-27 18:13:11 +0900

    💄 client/design: 모바일, 데스크탑 대응 디자인 적용

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,18 +1,32 @@
 import { EmailIcon } from "@chakra-ui/icons";
-import { Box } from "@chakra-ui/react";
+import { Box, HStack, Text, useBreakpointValue } from "@chakra-ui/react";
 
 const Footer = () => {
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
   return (
     <Box
       as="footer"
-      position="fixed"
-      bottom="0"
-      left="0"
-      width="100%"
+      w={isMobile ? "calc(100vw - 92px)" : "406px"}
       textAlign="center"
       paddingY="2"
+      justifyContent="center"
+      alignItems="center"
+      display="flex"
+      flexDir="column"
+      position="fixed"
+      bottom={isMobile ? "0" : "140"}
+      gap="1"
     >
-      <EmailIcon boxSize={4} />
+      <HStack spacing="1">
+        <EmailIcon boxSize={4} />
+        <Text fontSize="xs" fontWeight="bold" ml="1">
+          den.sjuhan.dev@gmail.com
+        </Text>
+      </HStack>
+      <Text fontSize="xs" fontWeight="bold" ml="1">
+        2023. All rights reserved.
+      </Text>
     </Box>
   );
 };
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,9 +1,23 @@
-import { Box, Center } from "@chakra-ui/react";
+import { Box, Center, useBreakpointValue } from "@chakra-ui/react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
   return (
     <Center h="100vh" w="100vw">
-      <Box p="46" w="100%" maxW="1200px" h="100%">
+      <Box
+        p="46"
+        w="500px"
+        h="800px"
+        style={
+          !isMobile
+            ? {
+                border: "1px solid #E2E8F0",
+                borderRadius: "10px",
+              }
+            : {}
+        }
+      >
         {children}
       </Box>
     </Center>
