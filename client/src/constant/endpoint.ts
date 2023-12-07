d3420ccfbd54ece1845bbe3e41ab3d9cb9f206ca
sjuhan123 <sjuhan123@gmail.com>
2023-12-07 15:45:37 +0900

    ♻️ client/refactor: endpoint BASE_URL 추가

--- client/src/constant/endpoint.ts
+++ client/src/constant/endpoint.ts
@@ -1,9 +1,11 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL || "";
+
 export const END_POINT = Object.freeze({
-  CLOSEST_WELFARES: `api/welfares/closest`,
-  DISTRICTS: `api/districts`,
-  USER: `api/user`,
-  KAKAO_LOGIN: `api/auth/kakao`,
-  KAKAO_LOGOUT: `api/user/logout`,
-  USER_WELFARE_BOOKMARK: `api/user/welfare`,
-  WELFARES: `api/welfares`,
+  CLOSEST_WELFARES: `${BASE_URL}api/welfares/closest`,
+  DISTRICTS: `${BASE_URL}api/districts`,
+  USER: `${BASE_URL}api/user`,
+  KAKAO_LOGIN: `${BASE_URL}api/auth/kakao`,
+  KAKAO_LOGOUT: `${BASE_URL}api/user/logout`,
+  USER_WELFARE_BOOKMARK: `${BASE_URL}api/user/welfare`,
+  WELFARES: `${BASE_URL}api/welfares`,
 });
