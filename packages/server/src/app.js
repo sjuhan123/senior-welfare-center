ae22b1c3fd9f4466431ca97a15e2942e844778a8
sjuhan123 <sjuhan123@gmail.com>
2024-04-06 14:47:56 +0900

    ✨ server/feat: remove unuse code at app.js

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -1,14 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-// import path from 'path';
 import helmet from 'helmet';
-// import { fileURLToPath } from 'url'; // URL에서 파일 경로로 변환하는 함수
 import morgan from 'morgan';
 
 import api from './routes/api.js';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -49,11 +44,5 @@ app.use(morgan('combined'));
 app.use(express.json());
 
 app.use('/api', api);
-
-// 프론트엔드 정적 파일 제공
-// app.use(express.static(path.join(__dirname, '..', 'public')));
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-// });
 
 export default app;
