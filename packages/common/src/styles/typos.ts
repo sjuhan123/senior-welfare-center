bd981c31e19d08fc78d1e0090893ad685269f3ce
sjuhan123 <sjuhan123@gmail.com>
2024-04-15 21:04:41 +0900

    ✨ common/feat: add typo
    - BODY_1_SEMIBOLD
    - BODY_1_BOLD
    - set storybook

--- packages/common/src/styles/typos.ts
+++ packages/common/src/styles/typos.ts
@@ -2,14 +2,28 @@ import { css } from '@emotion/react';
 
 const HEAD = css`
   font-weight: 700;
-  font-size: 1.5rem;
+  font-size: 23px;
   line-height: 138%;
   letter-spacing: -0.3px;
 `;
 
-const BODY = css`
+const BODY_1_REGULAR = css`
   font-weight: 400;
-  font-size: 24px;
+  font-size: 21px;
+  line-height: 150%;
+  letter-spacing: -0.3px;
+`;
+
+const BODY_1_SEMIBOLD = css`
+  font-weight: 500;
+  font-size: 21px;
+  line-height: 150%;
+  letter-spacing: -0.3px;
+`;
+
+const BODY_1_BOLD = css`
+  font-weight: 600;
+  font-size: 21px;
   line-height: 150%;
   letter-spacing: -0.3px;
 `;
@@ -23,7 +37,9 @@ const DETAIL = css`
 
 const typos = {
   HEAD,
-  BODY,
+  BODY_1_REGULAR,
+  BODY_1_SEMIBOLD,
+  BODY_1_BOLD,
   DETAIL,
 } as const;
 
