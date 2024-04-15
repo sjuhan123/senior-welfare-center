04a3746b506131c6aa6f77cb32c836a9eea240a8
sjuhan123 <sjuhan123@gmail.com>
2024-04-15 21:19:24 +0900

    ✨ client/feat: remove email icon and media query

--- packages/client/src/components/footer/Footer.tsx
+++ packages/client/src/components/footer/Footer.tsx
@@ -1,31 +1,20 @@
-import { EmailIcon } from '@chakra-ui/icons';
-import type { Theme } from '@emotion/react';
 import { css } from '@emotion/react';
 import type { PropsWithChildren } from 'react';
 
 const Footer = ({ children }: PropsWithChildren) => {
-  return (
-    <footer css={containerCss}>
-      <EmailIcon boxSize={4} />
-      {children}
-    </footer>
-  );
+  return <footer css={containerCss}>{children}</footer>;
 };
 
 export default Footer;
 
-const containerCss = (theme: Theme) => css`
+const containerCss = css`
   display: flex;
-  position: static;
+  position: fixed;
+  bottom: 0;
+  left: 0;
   justify-content: center;
   align-items: center;
 
   width: 100%;
   height: 80px;
-
-  @media (max-width: ${theme.size.maxWidth}) {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-  }
 `;
