0e15064abb09f61ad1491361590893f9bf9b1ca4
sjuhan123 <sjuhan123@gmail.com>
2024-04-15 21:07:09 +0900

    ✨ common/feat: Button component
    - set index.ts

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,11 +2,13 @@ import colors from './styles/colors';
 import defaultTheme from './styles/theme';
 import { ITheme } from './styles/types';
 import typos from './styles/typos';
+import maxWidths from './styles/maxWidths';
 
-import Text from './components';
+import Button from './components/button/Button';
 
 export type ThemeType = ITheme;
 export { colors, defaultTheme };
 export { typos };
+export { maxWidths };
 
-export { Text };
+export { Button };
