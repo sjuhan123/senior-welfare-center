bd07b0bd2f9d21977cf151d0a9815239b968ddc7
sjuhan123 <sjuhan123@gmail.com>
2024-04-17 18:27:45 +0900

    ✨ client/feat: welfareCard, welfareCardSkeleton
    - mock images for swiper
    - weflareCard component
    - welfareCardSkeleton component

---
+++ packages/client/src/components/card/WelfareCard.tsx
@@ -0,0 +1,110 @@
+import { css, useTheme } from '@emotion/react';
+import type { WelfareData } from '../../types/welfare';
+import { Swiper } from '../swiper/swiper';
+import type { Theme } from '@emotion/react';
+import { Button, typos } from '@common/shared';
+import { MdBookmarkAdded } from 'react-icons/md';
+import { useState } from 'react';
+
+interface Prop {
+  welfareInfo: WelfareData;
+  onCardClick?: () => void;
+  onBookmarkClick?: () => void;
+}
+
+const MOCK_IMAGES = [
+  '../../../../public/송정동-정보검색대.jpeg',
+  '../../../../public/송정동-2층복도쉼터.jpeg',
+  '../../../../public/송정동-강당.jpeg',
+  '../../../../public/송정동-건강관리실스마트.jpeg',
+  '../../../../public/송정동-배움터.jpeg',
+  '../../../../public/송정동-휴게실.jpeg',
+  '../../../../public/송정동-안내데스크.png',
+  '../../../../public/송정동-건강관리실.jpeg',
+];
+
+const WelfareCard = ({ welfareInfo, onCardClick, onBookmarkClick }: Prop) => {
+  const theme = useTheme();
+
+  const [isBookmarked, setisBookmarked] = useState(false);
+  const { name, address, distance, phone, images } = welfareInfo;
+
+  const WELFARE_IMAGES = images ?? MOCK_IMAGES;
+
+  const handleBookmarkClick = () => {
+    setisBookmarked(prev => !prev);
+    onBookmarkClick ? onBookmarkClick() : null;
+  };
+
+  return (
+    <div css={containerCss} onClick={onCardClick}>
+      <Swiper>
+        {WELFARE_IMAGES.map(info => (
+          <img key={info} src={info} alt="welfareImage" />
+        ))}
+      </Swiper>
+      <div css={bodyCss}>
+        <div css={infoCss}>
+          <h2>{name}</h2>
+          <p>주소 {address}</p>
+          <p>거리 {distance}km 거리</p>
+          <p>번호 {phone}</p>
+        </div>
+        <div css={buttonCss(theme, isBookmarked)} onClick={handleBookmarkClick}>
+          <Button css={typos.DETAIL_1_SEMIBOLD}>저장</Button>
+          {<MdBookmarkAdded size={20} />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WelfareCard;
+
+const containerCss = css`
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const bodyCss = css`
+  display: flex;
+  position: relative;
+  flex-direction: row;
+  padding: 12px 0 0 0;
+  width: 100%;
+`;
+
+const infoCss = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  h2 {
+    ${typos.DETAIL_1_BOLD};
+    color: ${theme.colors.black};
+  }
+
+  p {
+    ${typos.DETAIL_1_REGULAR};
+    color: ${theme.colors.black};
+  }
+`;
+
+const buttonCss = (theme: Theme, isBookmarked: boolean) => css`
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  gap: 3px;
+  position: absolute;
+  right: 0;
+
+  ${typos.DETAIL_1_BOLD}
+  color: ${isBookmarked ? theme.colors.blue_200 : theme.colors.black};
+
+  &:hover {
+    color: ${theme.colors.blue_200};
+  }
+`;
---
+++ packages/client/src/components/card/WelfareCardsSkeleton.tsx
@@ -0,0 +1,119 @@
+import type { Theme } from '@emotion/react';
+import { css } from '@emotion/react';
+
+interface Props {
+  cardTotal?: number;
+}
+
+const WelfareCardsSkeleton = ({ cardTotal = 4 }: Props) => {
+  return (
+    <section css={containerCss}>
+      <ul css={wrapperCss}>
+        {Array.from({ length: cardTotal }, (_, index) => (
+          <li key={index} css={cardContainerCss}>
+            <div css={cardImageWrapperCss}>
+              <div css={cardImageCss} />
+            </div>
+            <div css={cardBodyCss}>
+              <div css={cardInfoCss}>
+                <h2 />
+                <span />
+                <p />
+                <p />
+              </div>
+              <div css={buttonCss} />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default WelfareCardsSkeleton;
+
+const containerCss = (theme: Theme) => css`
+  display: grid;
+  overflow: auto;
+
+  padding: 20px;
+  background-color: ${theme.colors.white};
+  width: 100%;
+  height: calc(100vh - 277px);
+`;
+
+const wrapperCss = css`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 40px 24px;
+`;
+
+const cardContainerCss = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+const cardImageWrapperCss = (theme: Theme) => css`
+  height: 100%;
+  width: 100%;
+  background-color: ${theme.colors.gray_100};
+  border-radius: 10px;
+  animation: ${theme.animations.pulse} 2s infinite ease-in-out;
+`;
+
+const cardImageCss = css`
+  height: 253px;
+  width: 300px;
+`;
+
+const cardBodyCss = css`
+  display: flex;
+  position: relative;
+  flex-direction: row;
+  padding: 12px 0 0 0;
+  width: 100%;
+`;
+
+const cardInfoCss = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+
+  h2 {
+    width: 170px;
+    height: 20px;
+    background-color: ${theme.colors.gray_100};
+    border-radius: 10px;
+    animation: ${theme.animations.pulse} 2s infinite ease-in-out;
+  }
+
+  span {
+    width: 180px;
+    height: 20px;
+    background-color: ${theme.colors.gray_100};
+    border-radius: 10px;
+    animation: ${theme.animations.pulse} 2s infinite ease-in-out;
+  }
+
+  p {
+    width: 150px;
+    height: 20px;
+    background-color: ${theme.colors.gray_100};
+    border-radius: 10px;
+    animation: ${theme.animations.pulse} 2s infinite ease-in-out;
+  }
+`;
+
+const buttonCss = (theme: Theme) => css`
+  display: flex;
+  align-items: center;
+  width: 60px;
+  height: 20px;
+  position: absolute;
+  right: 0;
+  border-radius: 10px;
+
+  background-color: ${theme.colors.gray_100};
+  animation: ${theme.animations.pulse} 2s infinite ease-in-out;
+`;
