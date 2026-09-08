bdcb30894ead74c9dc3cee9a4a0c8e5b3acfc38e
sjuhan123 <sjuhan123@gmail.com>
2026-09-08 14:32:25 +0900

    ✨ admin/feat: Emotion 테마 연결
    packages/common의 디자인 토큰을 admin 전용 모양(admin 접두어 벗김)으로
    재구성한 theme.ts를 만들고, Emotion 모듈 확장으로 Theme 타입에 연결.
    main.tsx에서 ThemeProvider로 앱 전체를 감쌈.
    
    mobile과 달리 scale.admin은 고정 px라 사용자 배율 상태가 필요 없어서,
    mobile의 useStyles 같은 훅 없이 순수 정적 객체로 끝남.

--- packages/admin/src/main.tsx
+++ packages/admin/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
 
 import App from './App';
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
